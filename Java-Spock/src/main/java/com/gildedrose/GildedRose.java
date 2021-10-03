47ec65caec5dc665e7a5073223970a2dc51f65e1
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-10-03 10:42:37 +0530

    Reversed if condition

--- Java-Spock/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Spock/src/main/java/com/gildedrose/GildedRose.java
@@ -41,8 +41,10 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            // Do nothing
+                        } else {
+                            if (items[i].quality > 0) {
                                 this.decreaseQuality(items, i);
                             }
                         }
